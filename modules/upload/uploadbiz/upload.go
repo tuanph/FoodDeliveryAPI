659b5ad74d35bfd19dec6ab82e96501c001b2bd5
TUANPH-HOME-PC <tuanpham@blogicsystems.com>
2021-04-18 00:33:34 +0700

    remove comment code

--- modules/upload/uploadbiz/upload.go
+++ modules/upload/uploadbiz/upload.go
@@ -41,8 +41,8 @@ func (biz *uploadBiz) Upload(ctx context.Context, data []byte, folder, fileName 
 		folder = "img"
 	}
 
-	fileExt := filepath.Ext(fileName)                                // "img.jpg" => ".jpg"
-	fileName = fmt.Sprintf("%d%s", time.Now().Nanosecond(), fileExt) // 9129324893248.jpg
+	fileExt := filepath.Ext(fileName)
+	fileName = fmt.Sprintf("%d%s", time.Now().Nanosecond(), fileExt) // 9129324893248.jpg  // "img.jpg" => ".jpg"
 
 	img, err := biz.provider.SaveFileUploaded(ctx, data, fmt.Sprintf("%s/%s", folder, fileName))
 
@@ -52,13 +52,7 @@ func (biz *uploadBiz) Upload(ctx context.Context, data []byte, folder, fileName 
 
 	img.Width = w
 	img.Height = h
-	//img.CloudName = "s3" // should be set in provider
 	img.Extension = fileExt
-
-	//if err := biz.imgStore.CreateImage(ctx, img); err != nil {
-	//	// delete img on S3
-	//	return nil, uploadmodel.ErrCannotSaveFile(err)
-	//}
 
 	return img, nil
 }
